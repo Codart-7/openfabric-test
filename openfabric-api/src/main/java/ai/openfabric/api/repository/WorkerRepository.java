5b9b885e565046b50975316e1cbb9eb91e563bff
Codart-7 <iconmoa@gmail.com>
2023-04-22 23:41:36 +0100

    clear repo

--- openfabric-api/src/main/java/ai/openfabric/api/repository/WorkerRepository.java
+++
@@ -1,8 +0,0 @@
-package ai.openfabric.api.repository;
-
-import ai.openfabric.api.model.Worker;
-import org.springframework.data.repository.CrudRepository;
-
-public interface WorkerRepository extends CrudRepository<Worker, String> {
-
-}
